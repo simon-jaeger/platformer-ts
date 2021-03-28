f194cf4b87203592ff4d82ee2c51b9a0fb8e86dd
Simon Jäger <simon-jaeger@users.noreply.github.com>
2021-03-28 15:30:38 +0200

    big boxes

--- src/actors/Box.ts
+++ src/actors/Box.ts
@@ -5,8 +5,14 @@ import Assets from "/src/services/Assets"
 export default class Box extends _Actor {
   onInitialize() {
     this.body.collider.type = CollisionType.Active
-    // language=file-reference
-    this.addDrawing(Assets.getTx('/assets/Box.png'))
+    if (this.width === t1) {
+      // language=file-reference
+      this.addDrawing(Assets.getTx('/assets/Box.png'))
+    }
+    if (this.width === t2) {
+      // language=file-reference
+      this.addDrawing(Assets.getTx('/assets/Box2.png'))
+    }
   }
 }
 
